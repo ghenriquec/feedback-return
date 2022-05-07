22cf00ff59722e9e1ef0cbb4d932d3df3162a319
Guilherme Henrique <guiihenriquecs@gmail.com>
2022-05-06 21:26:37 -0300

    feat: component to sucess page created

--- web/src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
@@ -1,5 +1,29 @@
-export function FeedbackSuccessStep (){
+import { CloseButton } from "../../CloseButton";
+import sucessImage from "../../../assets/success.svg";
+
+interface FeedbackSuccessStepProps {
+  onFeedbackRestartRequested: () => void;
+}
+
+export function FeedbackSuccessStep ({onFeedbackRestartRequested}: FeedbackSuccessStepProps){
   return (
-    <div></div>
+    <>
+    <header>
+      <CloseButton />
+    </header>
+
+    <div className="flex flex-col items-center py-10 w-{304px}">
+      <img src={sucessImage} alt="Sucesso" />
+      <span className="text-xl mt-8">Agradecemos seu feedback!</span>
+      <button
+      type="button"
+      onClick={onFeedbackRestartRequested}
+      className="py-2 px-6 mt-6 bg-zinc-800 rounded-md border-transparent text-sm leading-6 hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
+      >
+          Quero enviar outro!
+      </button>
+    </div>
+
+    </>
   )
 }
